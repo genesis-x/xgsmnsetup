8f16f8b72505826f81ea8db50a9a2775bf5ad882
testiface <39145197+testiface@users.noreply.github.com>
2018-05-27 19:20:25 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# nosmnsetup
+# NOS Masternode Install Script
+# Use this script on a fresh install of Ubuntu 16.04 - MUST BE 16.04 x64
+
+Stable and cheap host here https://m.do.co/c/b93026fab810
+
+NOS address if this helped you and you love me: NWrcgc4eSAqrrp5J7XUGiTvY7bZCs9Nify
+
+
+# Use this script on a fresh install of Ubuntu 16.04 - MUST BE 16.04 x64
+
+# Part 1 - Sending Collateral Coins
+
+1. Open your Windows wallet - MAKE SURE IT IS SYNCED WITH THE NETWORK
+2. Go to Tools -> Debug Console
+3. Type: getaccountaddress MN# (# is your masternode number you want to use)
+4. Send 35000 NOS to this address
+5. Go to Tools -> Debug Console
+6. Type: masternode outputs (This can take a minute before an output is shown)
+7. Type: masternode genkey
+7. Save your TX ID (The first number) and your Index Number (Second number, either a 1 or 0)
+8. Save your generated key as well as this will be needed in your VPS as your private key
+9. Save these with a notepad
+10. Close the wallet
+11. Move to Part 2 for now
+
+# Part 2 - Getting your Linux VPS Started Up (Read all instructions and follow prompts closely)
+
+1. Connect to your linux vps AS ROOT (AWS USERS USE sudo -i TO LOGIN AS ROOT), copy and paste the following line into your VPS.  Double click to highlight the entire line, copy it, and right click into Putty or Shift + Insert to paste.
+```
+cd && sudo apt-get -y install git && sudo git clone https://github.com/testiface/nosmnsetup.git && cd nosmnsetup/ && sudo bash install.sh 
+```
+2. follow the prompts closely and don't mess it up!
+3. Move to Part 3
+
+# Part 3 - Editing your Windows Config File
+
+1. Open your wallet
+2. Go to Tools -> Open Masternode Configuration File
+3. Enter the following on one single line after the example configuration
+```<alias> <ip>:7272 <private_key> <tx_id> <index>```
+4. It should look something like this:
+``` MN1 66.65.43.32:7272 87dfjnKNfdjNlwomdmKKMdkaNIE a3eofJJkdlMlfKokfmalmofO 0```
+5. Save and close the file and restart your wallet.
+
+# Part 4 - Starting the Masternode
+
+1. In your wallet, go to Tools -> Debug Console
+2. Enter ```startmasternode alias 0 <alias>``` with ```<alias>``` being the name of your masternode from Part 3
+3. Enjoy!  You can start this process over again for another MN on a fresh Linux VPS!
+
+# Part 5 - Checking Masternode Status
+
+1. After running the command in step 4, go back to your VPS
+2. Enter ```cd``` to get back to your root directory
+3. Enter ```marble-cli masternode status```
+4. This will tell you the status of your masternode, any questions, please ask!
+
+# Bronate if this helped
+
+*Official NOS Discord: https://discord.gg/US3t6aj
+
+MY NOS ADDRESS: NWrcgc4eSAqrrp5J7XUGiTvY7bZCs9Nify
+
+*Discord in case you need help from me personally: https://discord.gg/MwE4Fuc
+  - I support multiple coins, please state which you need help with
+
+
+# Recommended Tools
+
+- Putty - Easy to use and customizable SSH client.
+- SuperPutty - This allows you to have multiple Putty tabs open in one window, allowing you to easily organize and switch between masternode servers.
+
---
+++ install.sh
@@ -0,0 +1,97 @@
+#/bin/bash
+
+echo "*******************************"
+echo "*                             *"
+echo "*       NOS Masternode        *"
+echo "*           SETUP             *"
+echo "*            BY               *"
+echo "*           Tpot              *"
+echo "*                             *"
+echo "*                             *"
+echo "*******************************"
+echo ""
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!                                                 !"
+echo "! Make sure you double check before hitting enter !"
+echo "!                                                 !"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo && echo && echo
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+IP==$(curl -s4 icanhazip.com)
+echo -e "${GREEN}Please enter your private key: (Copy from Windows and right click to paste and press enter)${NC}" 
+read KEY
+sleep 2
+echo ""
+echo ""
+echo -e "${RED}PLEASE KEEP YOUR HANDS AND FEET INSIDE THE RIDE AT ALL TIMES!${NC}"
+sleep 5
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install wget nano unrar unzip -y
+sudo apt-get install libboost-all-dev libevent-dev software-properties-common -y
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+sudo apt-get install libpthread-stubs0-dev -y
+sudo apt-get install libzmq3-dev -y
+sudo apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
+libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git curl libdb4.8-dev \
+bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev libdb5.3++ unzip libzmq5
+
+sleep 2
+echo -e "${GREEN}Downloading more ram!${NC}"
+sleep 5 
+cd /var
+sudo touch swap.img
+sudo chmod 600 swap.img
+sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+sudo mkswap /var/swap.img
+sudo swapon /var/swap.img
+sudo free
+sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+cd
+sleep 2
+echo -e "${GREEN}Keeping out all the ${RED}HAXORZ!${NC}"
+sleep 5
+sudo apt-get install -y ufw
+sudo ufw allow ssh/tcp
+sudo ufw limit ssh/tcp
+sudo ufw allow 7272/tcp
+sudo ufw allow 7271/tcp
+sudo ufw logging on
+echo "y" | sudo ufw enable
+sleep 2
+echo ""
+echo ""
+echo -e "${GREEN}Moving stuff where it ${RED}NEEDS${NC} to be"
+sudo chmod +x /root/nosmnsetup/nosd /root/nosmnsetup/nos-cli
+sudo mv /root/nosmnsetup/nosd /root/nosmnsetup/nos-cli /usr/local/bin
+echo ""
+echo ""
+sleep 2
+echo -e "${GREEN}Creating the ${RED}Configuration File Now${NC}"
+sleep 2
+sudo mkdir /root/.nos
+echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> /root/.nos/nos.conf
+echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> /root/.nos/nos.conf
+echo "rpcallowip=127.0.0.1" >> /root/.nos/nos.conf
+echo "listen=1" >> /root/.nos/nos.conf
+echo "server=1" >> /root/.nos/nos.conf
+echo "daemon=1" >> /root/.nos/nos.conf
+echo "maxconnections=250" >> /root/.nos/nos.conf
+echo "masternode=1" >> /root/.nos/nos.conf
+echo "masternodeaddr$IP:7788" >> /root/.nos/nos.conf
+echo "externalip$IP:7788" >> /root/.nos/nos.conf
+echo "masternodeprivkey=$KEY" >> /root/.nos/nos.conf
+echo -e "${GREEN}STARTING THE DAEMON${NC}"
+cd /root
+sudo nosd
+sleep 2
+echo ""
+echo ""
+echo -e "Thank you for installing the new and improved Marble wallet.  Please move onto the ${RED}NEXT${NC} step."
